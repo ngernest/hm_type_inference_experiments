b0e50dda95678ec29a3c61c4f7b8d65dfd0a7317
Ernest Ng <eyn5@cornell.edu>
2025-04-20 10:59:10 -0400

    add type annotations

--- lib/typecheck.ml
+++ lib/typecheck.ml
@@ -22,15 +22,15 @@ type context = (string * typ) list
 exception TypeError of string
 
 (* generates fresh type variable *)
-let next_var_id = ref 0
+let next_var_id : int ref = ref 0
 
-let fresh_var () =
+let fresh_var () : typ =
   let id = !next_var_id in
   incr next_var_id;
   TVar id
 
 (* converts a type to string for display *)
-let rec string_of_type t =
+let rec string_of_type (t : typ) : string =
   match t with
   | TInt -> "int"
   | TBool -> "bool"
@@ -45,7 +45,7 @@ let rec string_of_type t =
 (* 'a, 'b, ... *)
 
 (* checks if a type variable occurs in a type *)
-let rec occurs var_id t =
+let rec occurs (var_id : int) (t : typ) : bool =
   match t with
   | TVar n -> n = var_id
   | TFun (t1, t2) -> occurs var_id t1 || occurs var_id t2
@@ -56,14 +56,14 @@ let rec occurs var_id t =
 type sub = (int * typ) list
 
 (* applies a substitution to a type *)
-let rec apply_subst (subst : sub) t =
+let rec apply_subst (subst : sub) (t : typ) : typ =
   match t with
   | TVar n -> ( try List.assoc n subst with Not_found -> t)
   | TFun (t1, t2) -> TFun (apply_subst subst t1, apply_subst subst t2)
   | _ -> t
 
 (* composes substitutions *)
-let compose_subst s1 s2 : sub =
+let compose_subst (s1 : sub) (s2 : sub) : sub =
   let s2' = List.map (fun (var, t) -> (var, apply_subst s1 t)) s2 in
   s1 @ List.filter (fun (var, _) -> not (List.mem_assoc var s1)) s2'
 
@@ -71,13 +71,14 @@ let compose_subst s1 s2 : sub =
 
 (* 
  * Implementation of Robinson's unification algorithm.
- * Returns a substitution that most weakly unifies the constraint (t1 = t2) for types t1, t2.
+ * Returns a substitution that most weakly unifies the constraint (t1 = t2) 
+* for types t1, t2.
  *)
 let rec unify t1 t2 : sub = failwith "not implemented"
 
-(* lookup a variable in the context *)
-(* raises TypeError if variable is not found *)
-let rec lookup ctx x =
+(* Looks-up a variable in the context, returning its type *)
+(* Raises TypeError if variable is not found *)
+let rec lookup (ctx : context) (x : string) : typ =
   match ctx with
   | [] -> raise (TypeError ("Unbound variable: " ^ x))
   | (y, t) :: rest -> if x = y then t else lookup rest x
@@ -88,12 +89,14 @@ let rec lookup ctx x =
  * Infers the type of expression e in context ctx via unification.
  * Returns a pair (t, s) where:
  *  - t is the inferred type of e
- *  - s is a substitution containing all the constraints that must be satisfied for e to be of type t.
+ *  - s is a substitution containing all the constraints that must be 
+ *    satisfied for e to be of type t.
  *)
-let rec infer (ctx : context) e : typ * sub = failwith "not implemented"
+let rec infer (ctx : context) (e : expr) : typ * sub =
+  failwith "not implemented"
 
 (* Main typechecking function that returns the inferred type *)
-let typecheck ctx e =
+let typecheck (ctx : context) (e : expr) : typ =
   try
     let t, s = infer ctx e in
     apply_subst s t
